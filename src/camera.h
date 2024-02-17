dd14aa2ce1c4228757792844c5a2a600d6d9f28f
Quentin <quentin.tourette@gmail.com>
2024-02-17 18:39:58 +0100

    diffuse material + lambertian diffuse

--- src/camera.h
+++ src/camera.h
@@ -14,6 +14,7 @@ public:
     double aspect_ratio = 1.0;
     int image_width = 100;
     int samples_per_pixel = 10;
+    int max_depth = 10;
 
     void render(const hittable &world)
     {
@@ -31,7 +32,7 @@ public:
                 for (int sample = 0; sample < samples_per_pixel; ++sample)
                 {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
 
                 write_color(std::cout, pixel_color, samples_per_pixel);
@@ -70,13 +71,17 @@ private:
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 
-    color ray_color(const ray &r, const hittable &world) const
+    color ray_color(const ray &r, int depth, const hittable &world) const
     {
+        if (depth <= 0)
+            return color(0.0, 0.0, 0.0);
+
         hit_record rec;
 
-        if (world.hit(r, interval(0, infinity), rec))
+        if (world.hit(r, interval(0.001, infinity), rec))
         {
-            return 0.5 * (rec.normal + color(1, 1, 1));
+            vec3 bounced_direction = rec.normal + random_unit_vector();
+            return 0.1 * ray_color(ray(rec.point_of_contact, bounced_direction), depth - 1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,10 @@ int main()
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 400;
     cam.samples_per_pixel = 100;
+    cam.max_depth = 50;
 
     cam.render(world);
 }
+
+// TODO
+// check why images are better stored in gamma space
